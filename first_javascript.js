1db1578501fec791fe592d49c6fd345a897c7a67
sakhlane <786mohamedsakhlanepasha@gmail.com>
2024-09-04 01:30:40 +0530

    file modified

--- first_javascript.js
+++ first_javascript.js
@@ -1,1 +1,13 @@
- 
+ console.log('hello javascript');
+
+//  javascript claculation 
+//   - +  equal preority
+//   * / equal preority 
+
+// it calculate what expression comes first 
+
+console.log(8 + 3 * 4 / 2) // 14
+
+console.log( 10 - 8 * 20 + 10 / 2 ) // -145
+
+console.log( 10 + 20 / 2 - 30 * 5 ) // -130
